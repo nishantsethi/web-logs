23345aa2dc784d7dd6090562b8b6d47062ac80ab
Nishant Sethi <sethi.nishant42@gmail.com>
2020-09-20 23:37:41 +0530

    FIxed issues with multiple logs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from time import sleep
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from math import sqrt
 from pathlib import Path
 import json
@@ -28,15 +28,22 @@ for i in logs_list:
 print(logs_list)
 no_of_logs = len(logs_list)
 
-log_id = '2p'
+log_id = '1p'
+
 
 # Run this when a person calls a function in their program
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    log_name = logs_list[0]['logName']
     # render the template (below) that will use JavaScript to read the stream
-    global log_id
-    log_id = '1p'
-    return render_template('index.html', logs_list = logs_list, log_id = log_id)
+    if request.method == 'POST':
+        text = request.form['text']
+        global log_id
+        log_id = text
+        print("yolyo " + log_id)
+        selected_dic =  next(item for item in logs_list if item["ID"] == log_id)
+        log_name = selected_dic['logName']
+    return render_template('index.html', logs_list = logs_list, log_id = log_id, log_name = log_name)
 
 
 
