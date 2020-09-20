e2233d701b270f8da308a227474e61afb8f941e2
Nishant Sethi <sethi.nishant42@gmail.com>
2020-09-20 18:56:38 +0530

    Added working for multiple  logs

--- main.py
+++ main.py
@@ -18,19 +18,36 @@ json_path = "data.json"
 data_json = json.load(open(json_path))
 
 logs_list = data_json['logs']    #List with all the logs info in each dict [{}{}]
+print(logs_list)
+
+tom = 1
+for i in logs_list:
+    i['ID'] = str(tom) + 'p'
+    tom = tom + 1
+
+print(logs_list)
 no_of_logs = len(logs_list)
 
+log_id = '2p'
 
 # Run this when a person calls a function in their program
 @app.route('/')
 def index():
     # render the template (below) that will use JavaScript to read the stream
-    return render_template('index.html', logs_list = logs_list)
+    global log_id
+    log_id = '1p'
+    return render_template('index.html', logs_list = logs_list, log_id = log_id)
 
-@app.route('/stream_sqrt')
+
+
+
+@app.route('/' + str(log_id))
 def stream():
     def generate():
-        with open('example.log') as f:
+        selected_dic = next(item for item in logs_list if item["ID"] == log_id)
+        log_path = selected_dic['logPath']
+        print(log_id)
+        with open(log_path) as f:
             while True:
                 yield f.read()
                 sleep(1)
