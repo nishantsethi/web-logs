922386622ef694415b01cca662da6188aee9330c
Nishant Sethi <sethi.nishant42@gmail.com>
2020-09-24 18:57:44 +0530

    package
    This adds functionality of a package

---
+++ web-logs-pkg/data/app.py
@@ -0,0 +1,72 @@
+from time import sleep
+from flask import Flask, render_template, request
+from math import sqrt
+from pathlib import Path
+import json
+import os
+
+app = Flask(__name__)
+
+
+class WebLogs:
+    current_working_dir = str(Path.cwd())
+    
+    def __init__(self, json_path: str):
+        self.json_path = json_path
+        self.__create_file(self.current_working_dir)
+
+    def __create_file(self, current_working_dir):
+        if os.path.exists(self.current_working_dir + "/templates"):
+            print("templates exists")
+        else:
+            print("Does not exist")
+
+
+json_path = "data.json"
+data_json = json.load(open(json_path))
+
+logs_list = data_json['logs']    #List with all the logs info in each dict [{}{}]
+print(logs_list)
+
+tom = 1
+for i in logs_list:
+    i['ID'] = str(tom) + 'p'
+    tom = tom + 1
+
+print(logs_list)
+no_of_logs = len(logs_list)
+
+log_id = '1p'
+
+
+# Run this when a person calls a function in their program
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    log_name = logs_list[0]['logName']
+    # render the template (below) that will use JavaScript to read the stream
+    if request.method == 'POST':
+        text = request.form['text']
+        global log_id
+        log_id = text
+        print("yolyo " + log_id)
+        selected_dic =  next(item for item in logs_list if item["ID"] == log_id)
+        log_name = selected_dic['logName']
+    return render_template('index.html', logs_list = logs_list, log_id = log_id, log_name = log_name)
+
+
+
+
+@app.route('/' + str(log_id))
+def stream():
+    def generate():
+        selected_dic = next(item for item in logs_list if item["ID"] == log_id)
+        log_path = selected_dic['logPath']
+        print(log_id)
+        with open(log_path) as f:
+            while True:
+                yield f.read()
+                sleep(1)
+
+    return app.response_class(generate(), mimetype='text/plain')
+
+app.run()
--- web-logs-pkg/web-logs.py
+++ web-logs-pkg/web-logs.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import json
 import os
 import logging
+from shutil import copyfile
 
 
 def main():
@@ -18,52 +19,92 @@ def main():
 
     # Variables
     json_path = args.path_to_json
-    current_working_dir = str(Path.cwd())
-
-    #Logging
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-
-    create_dir(current_working_dir)
-
-
-    def create_dir(current_working_dir):
-        temp_dir = current_working_dir + "/templates"
-        static_dir = current_working_dir + "/static"
-        
-        
-        if check_dir_exist(temp_dir):
-            logger.error("Template folder could not be created as a folder with name \"templates\" already exists, \
-                Please delete or rename the folder.")
-        else:
-            os.makedirs(temp_dir)
-            logger.debug("The Template folder has been created")
-            if check_dir_exist(static_dir):
-                logger.debug("The Template folder has been created")
-            else:    
-                logger.error("An attempt was made to create the \"templates\" folder but was unsuccesfull. Please make \
-                    sure the directory has write permissions.")
-
-
-        
-        if check_dir_exist(static_dir):
-            logger.error("The static folder could not be created as a folder with name \"static\" already exists, \
-                Please delete or rename the folder.")
-        else:
-            os.makedirs(static_dir)
-            if check_dir_exist(static_dir):
-                logger.debug("The static folder has been created")
-            else:    
-                logger.error("An attempt was made to create the \"static\" folder but was unsuccesfull. Please make \
-                    sure the directory has write permissions.")
     
-    def check_dir_exist(directory):
-        return os.path.exists(directory)
+    
+
+    
+    
+    class WebLogs:
+        current_working_dir = str(Path.cwd())
+        root_dir = os.path.abspath(os.path.dirname(__file__))
+        
+        def __init__(self, json_path: str):
+            self.json_path = json_path
+            self.logger = logging.getLogger(__name__)
+            self.logger.setLevel(logging.DEBUG)
+            self.__create_dir(self.current_working_dir)
+            
 
 
+        def __create_dir(self, current_working_dir):
+            temp_dir = self.current_working_dir + "/templates"
+            static_dir = self.current_working_dir + "/static"
+            
+            
+            if self.__check_dir_exist(temp_dir):
+                self.logger.error("Template folder could not be created as a folder with name \"templates\" already exists, \
+                    Please delete or rename the folder.")
+            else:
+                os.makedirs(temp_dir)
+                self.logger.debug("The Template folder has been created")
+                if self.__check_dir_exist(static_dir):
+                    self.logger.debug("The Template folder has been created")
+                else:    
+                    self.logger.error("An attempt was made to create the \"templates\" folder but was unsuccesfull. Please make \
+                        sure the directory has write permissions.")
 
 
+            
+            if self.__check_dir_exist(static_dir):
+                self.logger.error("The static folder could not be created as a folder with name \"static\" already exists, \
+                    Please delete or rename the folder.")
+            else:
+                os.makedirs(static_dir)
+                if self.__check_dir_exist(static_dir):
+                    self.logger.debug("The static folder has been created")
+                else:    
+                    self.logger.error("An attempt was made to create the \"static\" folder but was unsuccesfull. Please make \
+                        sure the directory has write permissions.")
+                    
+            
+        def __create_files(self,current_working_dir):
+            """ Copies the files from data folder in package to \
+                users working directory"""
 
+            current_working_dir = self.current_working_dir
 
+            temp_dir_local = self.current_working_dir + "/templates"
+            static_dir_local = self.current_working_dir + "/static"
+            app_dir_local = self.current_working_dir 
+            
+            index_dir_root = self.__get_data("index.html")
+            style_dir_root = self.__get_data("style.css")
+            app_dir_root = self.__get_data("app.py")
+            
+            ## Copy index.html
+            try:
+                copyfile(index_dir_root, temp_dir_local)
+            except:
+                self.logger.error('An attempt was made to copy "index.html" file but was unsuccesfull. Please make \
+                        sure the directory has write permissions.')
 
-main()
+            ## Copy Style.css
+            try:
+                copyfile(style_dir_root, static_dir_local)
+            except:
+                self.logger.error('An attempt was made to copy "index.html" file but was unsuccesfull. Please make \
+                        sure the directory has write permissions.')
+            
+            ## Copy App.py
+            try:
+                copyfile(app_dir_root, app_dir_local)
+            except:
+                self.logger.error('An attempt was made to copy "index.html" file but was unsuccesfull. Please make \
+                        sure the directory has write permissions.')
+            
+
+        def __get_data(self, fileName):
+            return os.path.join(self.root_dir, 'data', fileName)
+
+        def __check_dir_exist(self, directory):
+            return os.path.exists(directory)
